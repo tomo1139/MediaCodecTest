06a7417027423aaef5096ba211fd1d8bb030b5c3
tomo1139 <tomo1139@gmail.com>
2020-02-15 21:35:24 +0900

    no message

--- MediaCodecTest/app/src/main/java/develop/tomo1139/mediacodectest/MainActivity.kt
+++ MediaCodecTest/app/src/main/java/develop/tomo1139/mediacodectest/MainActivity.kt
@@ -2,11 +2,14 @@ package develop.tomo1139.mediacodectest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import develop.tomo1139.mediacodectest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding
     }
 }
