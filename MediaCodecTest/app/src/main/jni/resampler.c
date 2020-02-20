5e4a2a9bf1cc3c5d489d0268a33c8906ad6e75cd
tomo1139 <tomo1139@gmail.com>
2020-02-20 23:02:14 +0900

    no message

--- MediaCodecTest/app/src/main/jni/resampler.c
+++ MediaCodecTest/app/src/main/jni/resampler.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <jni.h>
 
 jstring Java_develop_tomo1139_mediacodectest_AudioResampler_resample(JNIEnv* env, jobject javaThis, jstring _inputFilePath, jstring _outputFilePath) {
@@ -19,14 +20,33 @@ jstring Java_develop_tomo1139_mediacodectest_AudioResampler_resample(JNIEnv* env
      short shortData;
      int dataIdx = 0;
      int lastWriteIdx = -1;
-     while (fread(&shortData, sizeof(shortData), 1, readFp)) {
-         dataIdx++;
-         int outputIdx = dataIdx * 44.1 / 48;
-         if (outputIdx != lastWriteIdx) {
-             lastWriteIdx = outputIdx;
-             fwrite(&shortData, sizeof(shortData), 1, writeFp);
-         }
+
+     fpos_t inputDataSize = 0;
+     fseek(readFp, 0L, SEEK_END);
+     fgetpos(readFp, &inputDataSize);
+     fseek(readFp, 0L, SEEK_SET);
+
+     short* pInputData = (short*)calloc(1, inputDataSize);
+     fread(pInputData, inputDataSize, 1, readFp);
+
+     int outputDataSize = inputDataSize * 44.1 / 48;
+     short* pOutputData = (short*)calloc(1, outputDataSize);
+
+     for (int i=0; i<outputDataSize/sizeof(short); i++) {
+        float targetPos = i * 48 / 44.1;
+        int iTargetPos = (int) targetPos;
+
+        int startData = pInputData[iTargetPos];
+        int endData = 0;
+        if (iTargetPos + 1 < inputDataSize) {
+            endData = pInputData[iTargetPos+1];
+        }
+
+        int targetData = (endData - startData) * (targetPos - iTargetPos) + startData;
+        pOutputData[i] = targetData;
      }
+
+     fwrite(pOutputData, outputDataSize, 1, writeFp);
 
      fclose(readFp);
      fclose(writeFp);
